0369ee40cb780c57bd1fa944a2d594286da8b391
Rakesh Das <i.am.rakeshdas07@gmail.com>
2025-12-28 20:02:17 +0530

    operation added

---
+++ 4_conversionOperation.js
@@ -0,0 +1,44 @@
+//1
+let num=3
+let negNum=-num
+
+console.log(negNum);
+
+
+//2. Arithmetic Operation
+console.log(2+3);
+console.log(2-5);
+console.log(6*8);
+console.log(2**4);
+console.log(3/8);
+console.log(5%2);
+
+
+//3. String merge operation
+let str1="Hey"
+let str2=" Rakesh"
+
+console.log(str1 + str2);   //string only gets added
+
+//4. String to nmber conversion is confusing
+
+//First two console wroked fine
+console.log("1"+2);
+console.log(2+"5");
+
+//below two are completely different, 
+console.log("7"+3+5);   // o/p=  735 ; if string is first all others are treated as string
+console.log(9+4+"6");   // o/p=  136 ; if string is last the first number will get added than gets converted into string
+
+//5. Prefix and Postfix operation (++,--)
+
+let x=54
+
+console.log(++x);
+console.log(x);
+
+
+
+
+
+
