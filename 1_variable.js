8eb3e357c3aa46678848ab8d16062c43fb49992c
Rakesh Das <i.am.rakeshdas07@gmail.com>
2025-12-28 14:09:18 +0530

    variable

---
+++ 1_variable.js
@@ -0,0 +1,24 @@
+/*
+Variable can be declared in four ways in JS:
+modern ways-                  const and let 
+Older ways(Not recommended)-  var and automaticall
+*/
+
+const accountId=154454      //Const variable can't be changed/ altered and have block scope
+let accEmail="rak@gmail.com" // let also has block scope
+var accPassword="11656"     // var  has global scope 
+accCity="Jaipur"
+
+
+let address; //output would be undefined as the value is not defined to the variable
+// accountId=2   X NOt allowed
+accEmail= "nish@gmail.com"
+accPassword=65469846
+accCity="Dehradun"
+
+//two ways to print..
+// 1. using console.log()                   //can use one variable 
+// 2. using console.table([])              //can use multiple variable all at once to get the output in tabular way
+console.log(accountId);
+console.table([accountId,accEmail,accPassword,accCity,address])
+
