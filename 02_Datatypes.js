c9f9e2a59d21aec7cead1bfcb4190099b630620d
Rakesh Das <i.am.rakeshdas07@gmail.com>
2025-12-28 18:32:08 +0530

    datatypes added

---
+++ 02_Datatypes.js
@@ -0,0 +1,21 @@
+// Datatypes
+
+//Premetive datatypes =built in datatypes
+/*
+number
+BigInt 
+string = ""
+boolean = true
+null  = it's a standalone value and it's type is object
+undefined  = value has not been defined yet and it's type is undefined only
+symbol
+*/
+
+let x=5             //number
+let isEmpty =false   //boolean
+let name= "Rakesh"  //string
+let raj
+
+
+console.table([typeof x,typeof isEmpty,typeof name, typeof raj ,typeof null])
+
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # JavaScript_Learning
 Learning Javascript from Chai aur Javascript
 Here we will learn only things that are required
+
+For documents go to MDN javascipt - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types
+ec39.cs ECMA standard( original JS documentation) - https://tc39.es
